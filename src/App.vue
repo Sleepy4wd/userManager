241ff0fd3894172a896a9e814c015bd6be0bc140
duyuying <1464451093@qq.com>
2019-01-13 11:36:31 +0800

    饿了么ui的安装和登录页的页面

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="login-container">
+    <div class="form-container">
+      <h2>用户登录</h2>
+      <el-form
+        label-position="top"
+        label-width="80px"
+        :model="formData"
+      >
+        <el-form-item label="名称">
+          <el-input v-model="formData.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-input v-model="formData.password"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button
+        class="login-btn"
+        type="success"
+      >成功按钮</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: "app",
   // components: {
   //   HelloWorld
   // }
-}
+  data: function() {
+    return {
+      formData: {
+        name: "",
+        password: ""
+      }
+    };
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.login-container {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form-container {
+    width: 570px;
+    background-color: #fff;
+    padding: 40px;
+    box-sizing: border-box;
+    border-radius: 10px;
+    .login-btn {
+      width: 100%;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+//导入base.css
+import './assets/base.css'
+
 Vue.config.productionTip = false
 
 new Vue({
